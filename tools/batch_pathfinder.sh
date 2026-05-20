2f04919e6ae038d34bf26c0436be27a81e02f7b4
picklethecat1488-hue <picklethecat1488@gmail.com>
2026-05-19 17:10:56 -0700

    Delete tools directory

--- tools/batch_pathfinder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-SCRIPT_DIR="${0:A:h}"
-NUM_ITERATIONS=${1:-100}
-MASTER_OUTPUT="out/pathfinder_output.txt"
-
-# Run N attempts then concatenate all outputs into out/pathfinder_output.txt
-cat "$SCRIPT_DIR/pathfinder_variants.txt" | parallel -j6 --tag --line-buffer "$(which python)" src/pathfinder.py {} -n ${NUM_ITERATIONS}
-cat out/*/pathfinder_output.txt >> "$MASTER_OUTPUT"
